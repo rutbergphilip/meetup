cbb2dbca475b79019838b1194e6cb536ca8cc0f0
rutbergphilip <philiprutberg00@gmail.com>
2021-12-20 15:12:08 +0100

    random amount of description sentences

--- src/models/storage/seeder.ts
+++ src/models/storage/seeder.ts
@@ -4,17 +4,21 @@ import events from './events.storage';
 import Event from '../event';
 import * as faker from 'faker';
 
-for (let index = 0; index < 10; index++) {
-  const user: User = {
-    id: idGenerator(),
-    name: faker.name.findName(),
-  };
+((): void => {
+  for (let index = 0; index < 10; index++) {
+    const user: User = {
+      id: idGenerator(),
+      name: faker.name.findName(),
+    };
 
-  const meetup: Event = new Event(
-    faker.name.title(),
-    faker.lorem.paragraph(),
-    user
-  );
+    const meetup: Event = new Event(
+      faker.name.title(),
+      faker.lorem.paragraph(Math.floor(Math.random() * 3) + 1),
+      user
+    );
 
-  events.set(`event-${index}-${idGenerator()}`, meetup);
-}
+    events.set(`event-${index}-${idGenerator()}`, meetup);
+  }
+})();
+
+console.log(events);
